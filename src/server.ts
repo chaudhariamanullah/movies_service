cfdde8e95bdb2a57116b834be91d86e58d832a78
chaudhariamanullah <chaudhari.amanullah0@gmail.com>
2026-04-10 00:05:45 +0530

    Updated Port

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,8 @@ dotenv.config();
 
 import app from './app.js';
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Movie service running on port ${PORT}`)
 })
